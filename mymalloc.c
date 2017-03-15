a9c02212119b3bcc3ee9dcd60531b381c0866729
lukemills88@gmail.com <lukemills88@gmail.com>
2017-03-15 11:41:10 -0400

    Wrote alignBy8, free_list_next and free_list_begin

--- mymalloc.c
+++ mymalloc.c
@@ -14,6 +14,8 @@ typedef struct flist  {
 } *Flist;
 
 caddr_t allocate(size_t size);
+caddr_t findFreeChunk(size_t size);
+size_t alignBy8(size_t size);
 
 int main(int argc, char **argv){
 	size_t size;
@@ -24,6 +26,9 @@ int main(int argc, char **argv){
 
 void *my_malloc(size_t size){
 	Flist f;
+
+	size = alignBy8(size);
+
 	if(malloc_h == NULL){
 		malloc_h = allocate(size);
 		printf("TEMP - Top of heap is %#lx\n", malloc_h);
@@ -32,24 +37,61 @@ void *my_malloc(size_t size){
 		f->flink = NULL;
 		printf("TEMP - Size is %d\n", f->size);
 	}
+	printf("Looking for memory...\n");
+	f = (Flist) findFreeChunk(size);
+	if(f != NULL){
+	printf("TEMP - Memory chunk found of size %d\n", f->size);
+	}
 }
 
 void my_free(void *ptr);
 
-void *free_list_begin();
+void *free_list_begin(){
+	if(malloc_h == NULL){
+		printf("TEMP - No free list entries\n");
+		return NULL;
+	}
+	else{ return (void*) malloc_h; }
+}
 
-void *free_list_next(void *node);
+void *free_list_next(void *node){
+	Flist f;
+	f = (Flist) node;
+	if(f->flink != NULL){
+		return (void*) f->flink;
+	}
+	else{
+		return NULL;
+	}
+}
 
 void coalesce_free_list();
+
+caddr_t findFreeChunk(size_t size){
+	Flist f;
+	f = (Flist) free_list_begin();
+
+	while(f != NULL){
+		if(f->size >= size+8){
+			printf("Found memory!\n");
+			return (caddr_t) f;
+		}
+		else{
+			f = (Flist) free_list_next((void*) f);
+		}
+	}
+	return NULL;
+
+}
 
 caddr_t allocate(size_t size){
 	caddr_t h;
 	Flist f;
 	if(size > (SIZE-16)){
-		printf("TEMP - Called sbrk(%d)\n", size);
-		h = (caddr_t) sbrk(size);
+		printf("TEMP - Called sbrk(%d)\n", size+8);
+		h = (caddr_t) sbrk(size+8);
 		f = (Flist) h;
-		f->size = size;
+		f->size = size+8;
 	}
 	else{
 		printf("TEMP - Called sbrk(%d)\n", SIZE);
@@ -59,3 +101,35 @@ caddr_t allocate(size_t size){
 	}
 	return h;
 }
+
+size_t alignBy8(size_t size){
+	size += (-size) & (7);
+	return size;
+}
+
+/*
+ 8: 01000
+    10111
+	00111
+
+
+ 9: 01001 
+    10110
+	00110
+
+	01001
+  + 00110
+  -------
+    01111
+
+ 5: 00101
+    11010
+	00010
+
+	00101
+  + 00010
+  -------
+    00111
+
+16: 10000
+*/
